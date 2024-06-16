1372e127b3e032e1cf5503a388c95f8b2705eb04
pavelsmirnov77 <pavelsmirnov35rus@yandex.ru>
2024-06-16 20:57:46 +0300

    Реализация автообновления токена

--- bot.py
+++ bot.py
@@ -6,17 +6,53 @@ import os
 from dotenv import load_dotenv
 from googletrans import Translator
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import threading
 
 load_dotenv()
 
 API_URL = os.getenv("API_URL")
-API_TOKEN = os.getenv("API_TOKEN")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TOKEN_ID = "666c7b7ffa4fb388b4fd9726"
+ROTATE_TOKEN_URL = os.getenv("ROTATE_TOKEN_URL")
+COOKIE = os.getenv("COOKIE")
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 translator = Translator()
 
 saved_images = {}
+
+API_TOKEN = None
+
+def refresh_token():
+    global API_TOKEN
+    
+    rotate_token_url = ROTATE_TOKEN_URL
+    
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Cookie": COOKIE
+    }
+
+    try:
+        response = requests.post(rotate_token_url, headers=headers)
+        print(f"Status Code: {response.status_code}")
+        print(f"Response Text: {response.text}")
+        if response.status_code == 200:
+            new_token = response.json().get("token")
+            if new_token:
+                API_TOKEN = new_token
+                print("Новый токен успешно получен")
+            else:
+                print("Не удалось получить новый токен. Ответ не содержит token")
+        else:
+            print(f"Не удалось обновить токен. Код ошибки: {response.status_code}, Тело ответа: {response.text}")
+    except Exception as e:
+        print(f"Ошибка при запросе на обновление токена: {e}")
+
+def periodic_token_refresh(interval):
+    refresh_token()
+    threading.Timer(interval, periodic_token_refresh, [interval]).start()
 
 def translate_to_english(text):
     language = translator.detect(text).lang
@@ -38,12 +74,24 @@ def send_welcome(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == "generate_image":
-        bot.send_message(call.message.chat.id, "Отправьте мне описание и я нарисую по нему изображение.")
+        bot.send_message(call.message.chat.id, "Отправьте мне описание, и я нарисую по нему изображение.")
     elif call.data == "author_info":
         github_link = "https://github.com/pavelsmirnov77/telegram-bot-image-ai"
         bot.send_message(call.message.chat.id, f"Автор: Смирнов Павел\nGitHub: {github_link}")
+    elif call.data == "save_image":
+        user_id = call.message.chat.id
+        if user_id in saved_images:
+            image_data = saved_images[user_id]
+            bio = io.BytesIO(image_data)
+            bio.name = "image.png"
+            bot.send_document(user_id, bio)
+            bot.answer_callback_query(call.id, "Изображение сохранено!")
+        else:
+            bot.answer_callback_query(call.id, "Изображение не найдено для сохранения.")
+
 
 def query(payload):
+    global API_TOKEN
     headers = {"Authorization": f"Bearer {API_TOKEN}"}
     response = requests.post(API_URL, headers=headers, json=payload)
     if response.status_code != 200:
@@ -84,19 +132,6 @@ def handle_message(message):
     else:
         bot.reply_to(message, f"Не удалось получить изображение. {error_message}")
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    user_id = call.message.chat.id
-
-    if call.data == "save_image":
-        if user_id in saved_images:
-            image_data = saved_images[user_id]
-            bio = io.BytesIO(image_data)
-            bio.name = "image.png"
-            bot.send_document(user_id, bio)
-            bot.answer_callback_query(call.id, "Изображение сохранено!")
-        else:
-            bot.answer_callback_query(call.id, "Изображение не найдено для сохранения.")
-
 if __name__ == "__main__":
+    periodic_token_refresh(300) 
     bot.polling()
